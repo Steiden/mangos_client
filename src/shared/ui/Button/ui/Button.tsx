57b3067697a7f8956fa000bc85261be25ef92f30
Daniil <baldindaniilza@mail.ru>
2024-10-23 22:47:35 +0500

    Шапка для гостя

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -1,13 +1,19 @@
-'use client';
+"use client";
 
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    className?: string;
+	className?: string;
+	style?: "primary" | "secondary";
 };
 
-export const Button = () => {
-    return (
-        <button></button>
-    )
-}
+export const Button = ({ className, style = "primary", children, ...rest }: Props) => {
+	return (
+		<button
+			className={`${styles["button"]} ${styles["button--" + style]} ${className}`}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+};
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -5,8 +5,11 @@ import styles from "./Header.module.scss";
 import Link from "next/link";
 import { HeaderProfile } from "@/widgets/HeaderProfile/ui/HeaderProfile";
 import { SearchInput } from "@/shared/ui/SearchInput/ui/SearchInput";
+import { useUser } from "@/shared/hooks/useUser";
+import { Button } from "@/shared/ui/Button/ui/Button";
 
 export const Header = () => {
+	const { user, loading, error } = useUser();
 	const navItems = [
 		{
 			label: "Проекты",
@@ -40,10 +43,26 @@ export const Header = () => {
 				<div className={`${styles["header__right"]}`}>
 					<ul className={`${styles["header__list"]}`}>
 						{navItems.map((navItem) => (
-							<NavItem label={navItem.label} icon={navItem.icon} key={navItem.label} />
+							<NavItem
+								label={navItem.label}
+								icon={navItem.icon}
+								key={navItem.label}
+							/>
 						))}
 					</ul>
-					<HeaderProfile />
+					{user ? (
+						<HeaderProfile />
+					) : (
+						<div className={`${styles["header__auth-buttons"]}`}>
+							<Button className={`${styles["header__auth-button"]}`}>Вход</Button>
+							<Button
+								className={`${styles["header__auth-button"]}`}
+								style="secondary"
+							>
+								Регистрация
+							</Button>
+						</div>
+					)}
 				</div>
 			</header>
 		</div>
