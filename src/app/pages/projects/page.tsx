6cb158a5bb470ec870f1f576faf64d7acddf0cde
Daniil <baldindaniilza@mail.ru>
2024-11-07 00:48:20 +0500

    Фикс багов для деплоя

--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,10 +1,4 @@
 import { Projects } from "@/pages/projects/ui/Projects";
-import { Metadata } from "next";
-
-export const meta: Metadata = {
-	title: "Mangos | Проекты",
-	description: "Страница проектов",
-};
 
 export default function ProjectsPage() {
     return <Projects />
--- src/app/pages/test/page.tsx
+++ src/app/pages/test/page.tsx
@@ -1,10 +1,4 @@
 import { Test } from "@/pages/test/ui/Test";
-import { Metadata } from "next";
-
-export const meta: Metadata = {
-	title: "Mangos | Тест",
-	description: "Страница теста ui компонентов",
-};
 
 export default function TestPage() {
 	return <Test />;
